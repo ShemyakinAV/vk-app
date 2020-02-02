8398433282257931656090f3448f53d84a442c6f
AShemyakin <antonshemyakin97@gmail.com>
2020-02-02 19:59:08 +0300

    Updates

---
+++ precache-manifest.60e9aa111274f94149f1ce9a6a19af33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b079a56373ad59679247433b5a6347ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceff95dd94ee1d19ae01",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "cc3fac4ea17cb8da0430",
+    "url": "/static/css/main.e664a7aa.chunk.css"
+  },
+  {
+    "revision": "ceff95dd94ee1d19ae01",
+    "url": "/static/js/2.508a679a.chunk.js"
+  },
+  {
+    "revision": "cc3fac4ea17cb8da0430",
+    "url": "/static/js/main.c1fc0d69.chunk.js"
+  },
+  {
+    "revision": "6c907a781e160c27841c",
+    "url": "/static/js/runtime-main.438ee33c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
